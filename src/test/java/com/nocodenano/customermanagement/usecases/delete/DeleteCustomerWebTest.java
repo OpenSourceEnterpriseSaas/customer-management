563e5091130ce499044bfb80563d75baa5a42b6a
David King <moogaipan@protonmail.com>
2023-10-17 20:25:09 -0400

    Delete web test

--- src/test/java/com/nocodenano/customermanagement/usecases/delete/DeleteCustomerWebTest.java
+++ src/test/java/com/nocodenano/customermanagement/usecases/delete/DeleteCustomerWebTest.java
@@ -13,8 +13,12 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -26,7 +30,7 @@ public class DeleteCustomerWebTest {
     @Autowired
     private ObjectMapper mapper;
     @Test
-    void expectTo_DeleteCustomer() throws Exception {
+    void expectNotTo_DeleteCustomer() throws Exception {
         UUID existingCustomerUUID = UUID.randomUUID();
         Customer customer = MockCustomerData.createValid(
                 existingCustomerUUID,
@@ -34,12 +38,36 @@ public class DeleteCustomerWebTest {
                 "King",
                 "davidking@code.net");
 
-        String customerJSON = mapper.writeValueAsString(customer);
+        mockMvc.perform(delete("/customers/{customerId}", existingCustomerUUID)
+                        .contentType("application/json"))
+                .andDo(print())
+                .andExpect(content().string(containsString("404 NOT_FOUND Not Found")));
+    }
+
+    @Test
+    void expectTo_DeleteExistingCustomer() throws Exception {
+        // create the customer to delete first name
+        UUID existingCustomerUUID = createCustomerToUpdate();
+        assertThat(existingCustomerUUID).isNotNull();
 
         mockMvc.perform(delete("/customers/{customerId}", existingCustomerUUID)
+                        .contentType("application/json"))
+                .andDo(print())
+                .andExpect(content().string(containsString("status=204 NO_CONTENT")));
+    }
+
+    private UUID createCustomerToUpdate() throws Exception {
+        UUID customerIdentifier = UUID.randomUUID();
+        String existingCustomer = MockCustomerData.createCustomerAsJson(
+                customerIdentifier,
+                "David",
+                "King",
+                "davidking@code.net");
+        mockMvc.perform(post("/customers")
                         .contentType("application/json")
-                        .content(customerJSON))
+                        .content(existingCustomer))
                 .andDo(print())
                 .andExpect(status().isOk());
+        return customerIdentifier;
     }
 }
